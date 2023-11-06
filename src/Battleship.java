797543d41200b4e5b49cfacf1df16413deb29d08
Computer Science <computerscience@Locals-MacBook-Air.local>
2023-11-06 09:22:25 -1000

    Started adding in lose check functionality to be checked at the start of every turn.

--- src/Battleship.java
+++ src/Battleship.java
@@ -1,9 +1,31 @@
 
 public class Battleship {
+	int deadShipCounterOne = 0;
+	int deadShipCounterTwo = 0;
+	boolean playerTurn = true;
+	public void main(String[] args) {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		
+	
+		if(deadShipCounterOne == 5){ 
+			System.out.println("Player 2 has sunk all of Player 1's battleships!\nPlayer 1 wins!" );
+		}
 	}
 
-}
+	public boolean loseCheck(){
+		if (playerTurn == false) {
+			if (deadShipCounterOne == 5) {
+				return true;
+			}else
+				return false;
+		}else
+			if (deadShipCounterTwo == 5) {
+				return true;
+			}else 
+				return false;    
+		}
+	}
+	
+
+
