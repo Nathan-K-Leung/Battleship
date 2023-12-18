0675cd33526b9265f2491369ff0eee4d95aba20f
Computer Science <computerscience@Locals-MacBook-Air.local>
2023-12-18 08:31:01 -1000

    hi

--- src/Battleship.java
+++ src/Battleship.java
@@ -1,31 +1,70 @@
 
-public class Battleship {
-	int deadShipCounterOne = 0;
-	int deadShipCounterTwo = 0;
-	boolean playerTurn = true;
-	public void main(String[] args) {
-
+//public class Battleship {
+//	public static boolean playerTurn = true;
+//	static int deadShipCounterOne = 1;
+//	static int deadShipCounterTwo = 5;
+//	public static void main(String[] args) {
 	
+		//Clear screen, player 1/2 ready? 
+		
+		
+		
+		
+		
+
+public class Battleship {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Game game = new Game();
+		game.run();
+		
+		}
+}
+	
+	
+
+
+
+
+		
+		
+		
+		
+		
+		/*Turn Order:
+		 * 1. Check if someone lost.
+		 * 2. Current player (from playerTurn boolean) inputs an x and y to hit a ship.
+		 * 	2.1 If out of bounds, let them shoot again.
+		 *  2.2 If hit a spot they already fired at, let them shoot again. Use ShotRecord. 
+		 * 3. Ask ships if x y hit. Use orientation boolean to determine loop through ship length.
+		 * 	   Repeat for all ships. If hit any, print, "'You hit the '+ship name+!" 
+		 * 	   Add to ShotRecord. 
+		 * 4. If miss, add to ShotRecord.
+		 * 5. Check ShotRecord to ships 
+		 * 
+		 */
+
 		
 	
-		if(deadShipCounterOne == 5){ 
-			System.out.println("Player 2 has sunk all of Player 1's battleships!\nPlayer 1 wins!" );
-		}
-	}
+	 // 	if (loseCheck()==true) {
+	 
+	//		if (deadShipCounterOne == 5) {
+	//			System.out.println("Player 2 hit all ships! Player 2 wins!");
+	//		}else 
+	//			System.out.println("Player 1 hit all ships! Player 1 wins!");
+	//		}
+	//	}
 
-	public boolean loseCheck(){
-		if (playerTurn == false) {
-			if (deadShipCounterOne == 5) {
-				return true;
-			}else
-				return false;
-		}else
-			if (deadShipCounterTwo == 5) {
-				return true;
-			}else 
-				return false;    
-		}
-	}
+	
+	//public static boolean loseCheck(){
+		//	if (deadShipCounterOne == 5 || deadShipCounterTwo == 5) {
+		//		return true;
+		//	}else
+		//		return false; 
+	//	}
+	//}
+
 	
 
 
